c93f704c5a0745e3f401c6d8af32d1e997d793bf
boufkarasaleh <boufkarasaleh@gmail.com>
2021-08-30 21:49:59 +0100

    editing needs file

--- needs.py
+++ needs.py
@@ -9,42 +9,15 @@ logo = """
                    |___/                       
 """
 
-words = ["abruptly", "absurd", "abyss", "affix", "askew", "avenue", "awkward", "axiom", "azure", "bagpipes", "bandwagon", "banjo", "bayou", "beekeeper", "bikini", "blitz", "blizzard", "boggle", "bookworm", "boxcar", "boxful", "buckaroo", "buffalo", "buffoon", "buxom", "buzzard", "buzzing", "buzzwords", "caliph", "cobweb", "cockiness", "croquet", "crypt", "curacao", "cycle", "daiquiri", "dirndl", "disavow", "dizzying", "duplex", "dwarves", "embezzle", "equip", "espionage", "euouae", "exodus", "faking", "fishhook", "fixable", "fjord", "flapjack", "flopping", "fluffiness", "flyby", "foxglove", "frazzled", "frizzled", "fuchsia", "funny", "gabby", "galaxy", "galvanize", "gazebo", "giaour", "gizmo", "glowworm", "glyph", "gnarly", "gnostic", "gossip", "grogginess", "haiku", "haphazard", "hyphen", "iatrogenic", "icebox", "injury", "ivory", "ivy", "jackpot", "jaundice", "jawbreaker", "jaywalk", "jazziest", "jazzy", "jelly", "jigsaw", "jinx", "jiujitsu", "jockey", "jogging", "joking", "jovial", "joyful", "juicy", "jukebox", "jumbo", "kayak", "kazoo", "keyhole", "khaki", "kilobyte", "kiosk", "kitsch", "kiwifruit", "klutz", "knapsack", "larynx", "lengths", "lucky", "luxury", "lymph", "marquis", "matrix", "megahertz", "microwave", "mnemonic", "mystify", "naphtha", "nightclub", "nowadays", "numbskull", "nymph", "onyx", "ovary", "oxidize", "oxygen", "pajama", "peekaboo", "phlegm", "pixel", "pizazz", "pneumonia", "polka", "pshaw", "psyche", "puppy", "puzzling", "quartz", "queue", "quips", "quixotic", "quiz", "quizzes", "quorum", "razzmatazz", "rhubarb", "rhythm", "rickshaw", "schnapps", "scratch", "shiv", "snazzy", "sphinx", "spritz", "squawk", "staff", "strength", "strengths", "stretch", "stronghold", "stymied", "subway", "swivel", "syndrome", "thriftless", "thumbscrew", "topaz", "transcript", "transgress", "transplant", "triphthong", "twelfth", "twelfths", "unknown", "unworthy", "unzip", "uptown", "vaporize", "vixen", "vodka", "voodoo", "vortex", "voyeurism", "walkway", "waltz", "wave", "wavy", "waxy", "wellspring", "wheezy", "whiskey", "whizzing", "whomever", "wimpy", "witchcraft", "wizard", "woozy", "wristwatch", "wyvern", "xylophone", "yachtsman", "yippee", "yoked", "youthful", "yummy", "zephyr", "zigzag", "zigzagging", "zilch", "zipper", "zodiac", "zombie"]
+# You can use the words that you prefer
+words = ["awkward", "azure", "beekeeper", "bikini", "buzzard", "cycle", "funny", "galaxy", "joking", "jump", "length", "lucky", "matrix", "microwave", "oxygen", "pajama", "pixel", "puppy","quiz", "strength", "subway", "unknown", "unworthy", "unzip", "vodka", "wave", "whiskey", "wizard", "yummy", "zigzag", "zodiac", "zombie"]
 
-HANGMANPICS = ["""
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========""", """
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========""", """
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========""", """
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========""", """
+stages = ["""
   +---+
   |   |
   O   |
  /|\  |
-      |
+ / \  |
       |
 =========""", """
   +---+
@@ -58,11 +31,37 @@ HANGMANPICS = ["""
   |   |
   O   |
  /|\  |
- / \  |
+      |
+      |
+=========""", """
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========""", """
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========""", """
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========""", """
+  +---+
+  |   |
+      |
+      |
+      |
       |
 ========="""]
-
-
 
 
 
